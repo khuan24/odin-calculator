d7213c0bf1076e145200d2200d7ce3b28d1a6e97
Kexin Huang <khuan24@uwo.ca>
2025-01-14 18:22:01 -0500

    Connect button presses to operations

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const displayField = document.querySelector(".display")
-let currentNumber = 0
+let firstNumber = 0
+let secondNumber = 0
 let currentOp = ""
 
 function add(a, b) {
@@ -23,8 +24,8 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
-    // called when =, +, -, *, or / is pressed
-    const result = 0
+    // called when =, +, -, ×, or / is pressed
+    let result = 0
     switch (operator) {
         case "+": 
             result = add(a,b)
@@ -32,26 +33,32 @@ function operate(a, b, operator) {
         case "-":
             result = subtract(a,b)
             break;
-        case "*":
+        case "×":
             result = multiply(a,b)
             break;
         case "/":
             result = divide(a,b)
     }
 
-    // return a result to be displayed and stored as the next first number
+    // return a result to be displayed
     return result
 }
 
 function clear() {
     displayField.textContent = "0"
-    currentNumber = 0
+    firstNumber = 0
+    secondNumber = 0
     currentOp = ""
-
     const decimalBtn = document.querySelector("#decimal")
     decimalBtn.disabled = false
-    
 }
+
+// function enableAllButtons() {
+//     const btnList = document.querySelector("button")
+//     btnList.forEach((button) => {
+//         button.disabled = false
+//     })
+// }
 
 // listen for numbers to be pressed then display them
 const numberList = document.querySelectorAll(".number")
@@ -60,7 +67,7 @@ numberList.forEach((button) => {
         if (displayField.textContent.length == 10) {
             alert("You cannot enter more than 10 digits.")
         } else {
-            if (displayField.textContent == "0") {
+            if (displayField.textContent == "0" || displayField.textContent == firstNumber) {
                 displayField.textContent = ""
             }
     
@@ -70,6 +77,25 @@ numberList.forEach((button) => {
                 button.disabled = true
             }
         }
+    })
+})
+
+const operatorList = document.querySelectorAll(".operator") 
+operatorList.forEach((button) => {
+    button.addEventListener(("click"), () => {
+        if (currentOp == "") {
+            firstNumber = Number(displayField.textContent)
+            currentOp = button.textContent
+            clearDisplay = true
+        } else if (button.textContent == "=") {
+            secondNumber = Number(displayField.textContent)
+            displayField.textContent = operate(firstNumber, secondNumber, currentOp)
+            currentOp = ""
+            secondNumber = 0
+        }
+        
+    
+        
     })
 })
 
